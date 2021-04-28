e47e2c6351a80aac9bbb87e5c1f1ff4b1b7cdb13
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-04-28 19:13:19 +0900

    SampleScriptの課題が終了した。前期の課題の作業Sceneをそれぞれ追加した。

--- Assets/Scripts/SampleScript.cs
+++ Assets/Scripts/SampleScript.cs
@@ -24,50 +24,159 @@ public class SampleScript : MonoBehaviour
 
     void Update()
     {
+        SelectCube();
+        EraseCube();
+    }
+
+    public void SelectCube()
+    {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (m_selectIndex == cubes.Length - 1)
+            if (m_selectIndex == cubes.Length - 1 || !RightCubeIndexSearch(m_selectIndex))
             {
-                //return;
-    
                 cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.white;
-                cubes[0].GetComponent<Renderer>().material.color = Color.red;
+
                 m_selectIndex = 0;
 
+
+                while (!cubes[m_selectIndex].activeSelf)
+                {
+                    m_selectIndex++;
+                }
+
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+
                 Debug.Log(m_selectIndex);
-
-                return;
             }
+            else
+            {
+                RightCubeIndexSearch(m_selectIndex);
 
-            
-            cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.white;
-            m_selectIndex++;
-            cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
-            
-            Debug.Log(m_selectIndex);
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.white;
+
+                while (!cubes[m_selectIndex + 1].activeSelf)
+                {
+                    m_selectIndex++;
+                }
+                m_selectIndex++;
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+
+                Debug.Log(m_selectIndex);
+            }           
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if (m_selectIndex == 0)
+            if (m_selectIndex == 0 || !LeftCubeIndexSearch(m_selectIndex))
             {
-                //return;
-                
-                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.white;
-                cubes[cubes.Length - 1].GetComponent<Renderer>().material.color = Color.red;
 
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.white;
                 m_selectIndex = cubes.Length - 1;
 
+                while (!cubes[m_selectIndex].activeSelf)
+                {
+                    m_selectIndex--;
+                }
+
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+
                 Debug.Log(m_selectIndex);
-
-                return;
             }
+            else
+            {
+                LeftCubeIndexSearch(m_selectIndex);
 
-            cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.white;
-            m_selectIndex--;
-            cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.white;
 
-            Debug.Log(m_selectIndex);
+                while (!cubes[m_selectIndex - 1].activeSelf)
+                {
+                    m_selectIndex--;
+                }
+                m_selectIndex--;
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+
+                Debug.Log(m_selectIndex);
+            }
         }
+    }
+
+    public void EraseCube()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            cubes[m_selectIndex].SetActive(false);
+
+            if (m_selectIndex < cubes.Length - 1 && cubes[m_selectIndex + 1].activeSelf)
+            {
+                while (!cubes[m_selectIndex].activeSelf)
+                {
+                    m_selectIndex++;
+                }
+
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+                Debug.Log(m_selectIndex);
+            }
+            else if (m_selectIndex > 0 && cubes[m_selectIndex - 1].activeSelf)
+            {
+                while (!cubes[m_selectIndex].activeSelf)
+                {
+                    m_selectIndex--;
+                }
+
+                cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+                Debug.Log(m_selectIndex);
+            }
+            else
+            {
+                if (!RightCubeIndexSearch(m_selectIndex))
+                {
+                    while (!cubes[m_selectIndex - 1].activeSelf)
+                    {
+                        m_selectIndex--;
+                    }
+                    m_selectIndex--;
+                    cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+                }
+                else if (!LeftCubeIndexSearch(m_selectIndex))
+                {
+                    while (!cubes[m_selectIndex + 1].activeSelf)
+                    {
+                        m_selectIndex++;
+                    }
+                    m_selectIndex++;
+                    cubes[m_selectIndex].GetComponent<Renderer>().material.color = Color.red;
+                }
+            }
+        }
+    }
+
+    bool RightCubeIndexSearch(int currentIndex)
+    {
+        for (int i = currentIndex + 1; i < cubes.Length; i++)
+        {
+            if (cubes[i].activeSelf)
+            {
+                Debug.Log("Cubeがありました");
+
+                return true;
+            }
+        }
+        Debug.Log("Cubeが見つかりませんでした");
+
+        return false; 
+    }
+
+    bool LeftCubeIndexSearch(int currentIndex)
+    {
+        for (int i = currentIndex - 1; i >= 0; i--)
+        {
+            if (cubes[i].activeSelf)
+            {
+                Debug.Log("Cubeがありました");
+                return true;
+            }
+        }
+        Debug.Log("Cubeが見つかりませんでした");
+        return false;
     }
 }
