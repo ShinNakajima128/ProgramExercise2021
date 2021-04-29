8402644be62fd4b046ef407e6788f887406eee10
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-04-29 19:12:07 +0900

    MineSweeperSceneにフィールドを生成できるようにした。

---
+++ Assets/Scripts/MineSweeper/CellPanelController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellPanelController : MonoBehaviour
+{
+    [SerializeField] Transform m_camera;
+    RaycastHit hit;
+    GameObject targetcell;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        SelectCell();
+    }
+
+    public void SelectCell()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray,  out hit))
+        {
+            targetcell = hit.collider.gameObject;
+
+            if (targetcell.CompareTag("UpperCell"))
+            {
+                targetcell.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+        else
+        {
+            targetcell = null;
+        }
+    }
+    void SetCellStates(MineSweeperSystem.CellStates State)
+    {
+        
+    }
+    void Open()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MineSweeper/MineSweeperSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSweeperSystem : MonoBehaviour
+{
+    [SerializeField] int m_fieldSizeX = 10;             
+    [SerializeField] int m_fieldSizeY = 10;
+    [SerializeField] GameObject m_cellPrefab = null;
+    
+    /// <summary>
+    /// Cellの状態
+    /// </summary>
+    public enum CellStates
+    {
+        None,
+        Default,    //開いてない状態
+        Open        //開いてる状態
+    }
+
+    void Start()
+    {
+        GameObject[,] fieldCellObjects = new GameObject[m_fieldSizeY, m_fieldSizeX];
+
+        for (int i = 0; i < m_fieldSizeY; i++)
+        {
+            for (int n = 0; n < m_fieldSizeX; n++)
+            {
+                var cell = Instantiate(m_cellPrefab);
+                fieldCellObjects[n, i] = cell;
+                cell.transform.position = new Vector3(-0.5f + n, 0, -0.5f + i);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+}
