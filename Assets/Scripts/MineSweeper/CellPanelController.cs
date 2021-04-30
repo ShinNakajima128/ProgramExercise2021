f25c08a7337e149e9550f853fdaca2e553461eb3
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-04-30 22:08:57 +0900

    TitleSceneを作成し、GameSceneを選択してボタンを押すとそのSceneへ遷移するようにした。

--- Assets/Scripts/MineSweeper/CellPanelController.cs
+++ Assets/Scripts/MineSweeper/CellPanelController.cs
@@ -7,6 +7,8 @@ public class CellPanelController : MonoBehaviour
     [SerializeField] Transform m_camera;
     RaycastHit hit;
     GameObject targetcell;
+    [SerializeField] LayerMask targetLayer;
+    float maxDistance = 30.0f;
 
     void Start()
     {
@@ -21,17 +23,23 @@ public class CellPanelController : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray,  out hit))
+        if (Physics.Raycast(ray,  out hit, maxDistance, targetLayer))
         {
             targetcell = hit.collider.gameObject;
 
-            if (targetcell.CompareTag("UpperCell"))
+            targetcell.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            
+            if (targetcell.activeSelf && Input.GetMouseButtonDown(0))
             {
-                targetcell.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                targetcell.SetActive(false);
             }
         }
         else
         {
+            if (targetcell != null)
+            {
+                targetcell.GetComponent<Renderer>().material.color  = Color.green;
+            }
             targetcell = null;
         }
     }
---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] Dropdown m_sceneDropdowm = null;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(m_sceneDropdowm.captionText.text);
+    }
+}
