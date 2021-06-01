2a40a220759bbc73d6f194255a9255bbf4a5354f
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-06-01 20:03:37 +0900

    ReversiのCellPrefabを用意し、Reversiのプロトタイプを作成した。

---
+++ Assets/Scripts/Reversi/ReversiCell.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReversiCell : MonoBehaviour
+{
+    [SerializeField] GameObject m_whiteCell = null;
+    [SerializeField] GameObject m_blackCell = null;
+    [SerializeField] GameObject m_board = null;
+    [SerializeField] ReversiCellStates m_reversiCellStates = ReversiCellStates.None;
+
+    public enum ReversiCellStates
+    {
+        None,
+        White,
+        Black
+    }
+
+    public ReversiCellStates ReversiCellState
+    {
+        get => m_reversiCellStates;
+        set
+        {
+            m_reversiCellStates = value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (m_reversiCellStates)
+        {
+            case ReversiCellStates.None:
+                m_whiteCell.SetActive(false);
+                m_blackCell.SetActive(false);
+                break;
+            case ReversiCellStates.Black:
+                m_whiteCell.SetActive(true);
+                m_blackCell.SetActive(false);
+                break;
+            case ReversiCellStates.White:
+                m_whiteCell.SetActive(false);
+                m_blackCell.SetActive(true);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Reversi/ReversiSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReversiSystem : MonoBehaviour
+{
+    const int m_columns = 8;
+    const int m_rows = 8;
+    [SerializeField] ReversiCell m_ReversiCellPrefab = null;
+    ReversiCell[,] reversiCells = new ReversiCell[m_columns, m_rows];
+
+    void Start()
+    {
+        for (int i= 0; i < m_rows; i++)
+        {
+            for (int n = 0; n < m_columns; n++)
+            {
+                var cell = Instantiate(m_ReversiCellPrefab);
+                reversiCells[n, i] = cell;
+                cell.transform.position = new Vector3( n, 0, i);
+                if (n == 3 && i == 3 || n == 4 && i == 4) cell.ReversiCellState = ReversiCell.ReversiCellStates.White;
+                if (n == 4 && i == 3 || n == 3 && i == 4) cell.ReversiCellState = ReversiCell.ReversiCellStates.Black;
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
